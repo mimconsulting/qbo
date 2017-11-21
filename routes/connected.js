9dc34924ce85b88c70420ce7a043212fa6015b33
Anders Lyman <anderslyman@gmail.com>
2017-11-21 16:24:31 -0700

    Initial commit

---
+++ routes/connected.js
@@ -0,0 +1,50 @@
+var tools = require('../tools/tools.js')
+var https = require('https')
+var url = require('url')
+var express = require('express')
+var util = require('util')
+var router = express.Router()
+
+router.get('/', function (req, res) {
+  var token = tools.getToken(req.session)
+
+  if(!token) {
+    return res.redirect('/')
+  } 
+
+  // Don't call OpenID if we didn't request OpenID scopes
+  if(!tools.containsOpenId()) {
+    return res.render('connected')
+  }
+
+  // Call OpenID endpoint
+  // (this example uses the raw `https` npm module)
+  // (see api_call.js for example using helper `request` npm module)
+  var options = token.sign(url.parse(tools.openid_uri))
+  var request = https.request(options, (response) => {
+    response.setEncoding('utf8');
+    let rawData = '';
+    response.on('data', (chunk) => rawData += chunk);
+    response.on('end', () => {
+      console.log('OpenID response: ' + rawData)
+      try {
+        var parsedData = JSON.parse(rawData)
+        parsedData.id_token = ((token || {}).data || {}).id_token || ''
+        parsedData.refresh_token = ((token || {}).data || {}).refresh_token || ''
+        parsedData.access_token = ((token || {}).data || {}).access_token || ''
+        res.render('connected', parsedData)
+      } catch (e) {
+        console.log('ERROR', e.message)
+        res.render('connected')
+      }
+    });
+  });
+  request.end();
+
+  request.on('error', (e) => {
+    console.error(e)
+    res.send(e)
+  })
+})
+
+module.exports = router
